57218772f50758d480e59422ac048e8e3cf34026
Mathieu-Poirier <mathieuworkemail@gmail.com>
2024-12-21 21:38:57 -0500

    Added gitignore and new C exercises

--- theCProgrammingLanguage/simpleCProgram/build.md
+++ theCProgrammingLanguage/simpleCProgram/build.md
@@ -1,2 +1,2 @@
-clang FILE -Wall -Wextra -O0 -std=c99 -target x86_64-pc-windows-msvc -o pun.exe
+clang pun2.c -Wall -Wextra -O0 -std=c99 -target x86_64-pc-windows-msvc -o pun.exe
 splint pun.c
---
+++ theCProgrammingLanguage/simpleCProgram/pun2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("To C, or not to C: ");
+  printf("that is the question.\n");
+  return 0;
+}
+
+/*
+Here is a multi-line comment
+
+
+
+Hello
+*/
+
+/*
+ *
+ *
+ * Hello I am also a comment
+ *
+ */
+
+// C99 Comment
+
