bea4e944c0f08af2410f2fa07dd7dd64cd3bd5a9
Mathieu-Poirier <mathieuworkemail@gmail.com>
2024-12-26 21:27:28 -0500

    Did exercise 2.2

---
+++ theCProgrammingLanguage/exercises2/print.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // derective
+
+int main(void) {
+
+  printf("Parkinson's Law:\nWork expands so as to "); // statement
+  printf("fill the time\n");                          // statement
+  printf("available for its completion.\n");          // statement
+  return 0;                                           // statement
+}
+
+/*
+ * Output:
+ * Parkinson's Law:
+ * Work expands so as to fill the time
+ * available for its completion.
+ */
